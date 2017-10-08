6e3f65b99ec5e8fc640b4a2f79b28f06ff41f3ea
vroad <t.hiroya@gmail.com>
2017-10-08 23:39:15 +0900

    Disable https on ELB

--- web-service/elb/main.tf
+++ web-service/elb/main.tf
@@ -74,14 +74,6 @@ resource "aws_elb" "main" {
     instance_protocol = "http"
   }
 
-  listener {
-    lb_port            = 443
-    lb_protocol        = "https"
-    instance_port      = "${var.port}"
-    instance_protocol  = "http"
-    ssl_certificate_id = "${var.ssl_certificate_id}"
-  }
-
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
